b01698c0a3115c5f041ebf2014df9190e6acdf64
rolfjl <rolo@norceresearch.no>
2023-11-28 09:14:59 +0100

    Folder updated with setup and installation instructions.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Plotting',
+    version='1.0',
+    packages=['./'],
+    url='https://github.com/Python-Ensemble-Toolbox/Plotting',
+    license_files=('LICENSE.txt',),
+    author_email='rolo@norceresearch.no',
+    description='Useful plotting scripts for the Python Ensemble Toolbox',
+)
