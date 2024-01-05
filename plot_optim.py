af0e0cf64cc788c66aceae068d033e7ee8121025
rolfjl <rolo@norceresearch.no>
2024-01-05 14:36:12 +0100

    Fix plotting since ordering of variables has changed

--- plot_optim.py
+++ plot_optim.py
@@ -78,6 +78,7 @@ def plot_state(num_var):
         num_var = [num_var]  # make sure num_var is a list
     tot_var = sum(num_var)
     len_state = len(state_initial)
+    num_steps = int(len_state / tot_var)
     for i, k in enumerate(num_var):
 
         if len(num_var) >= i:
@@ -87,21 +88,22 @@ def plot_state(num_var):
         c = int(np.ceil(np.sqrt(num)))
         r = int(np.ceil(num / c))
         f, ax = plt.subplots(r, c, figsize=(10, 5))
+        ax = np.array(ax)
         ax = ax.flatten()
         for w in np.arange(num):
-            var_ini = state_initial[sum(num_var[0:i])+w:len_state:tot_var]
-            var_fin = state_final[sum(num_var[0:i])+w:len_state:tot_var]
+            var_ini = state_initial[sum(num_var[:i]) * num_steps + w:sum(num_var[:i + 1]) * num_steps:num]
+            var_fin = state_final[sum(num_var[:i]) * num_steps + w:sum(num_var[:i + 1]) * num_steps:num]
             ax[w].step(var_ini, '-b')
             ax[w].step(var_fin, '-r')
             ax[w].tick_params(labelsize=16)
             ax[w].set_xlabel('Index', size=18)
             ax[w].set_ylabel('State', size=18)
-            ax[w].set_title('Variable ' + str(w+1), size=18)
+            ax[w].set_title('Variable ' + str(w + 1), size=18)
             if w == 0:
                 ax[w].legend(['Initial', 'Final'], fontsize=16)
 
         f.tight_layout()
-        f.savefig(str(path_to_figures) + '/' + str(k))
+        f.savefig(str(path_to_figures) + '/variable_' + str(i))
         plt.show()
 
 
